5cc1de11460f76014bd363e12c36f8666828333d
sesamoel <sese13975@gmail.com>
2024-04-12 17:09:53 +0200

    first Frames

--- src/Terminal.java
+++ src/Terminal.java
@@ -14,12 +14,39 @@ public class Terminal{
     }
 
     public void run(String input){
-        switch(input){
+
+        String[] parts = input.split(" ");
+        String command = parts[0];
+
+
+        switch(command){
             case "info":
                 System.out.println("hier müssen alle befehle eingetragen werden, damit das hier das info menü ist.");
                 break;
 
+            case "create" : //Spieler erstellen
 
+                String playerName = "";
+
+                if(parts.length == 2){
+                    playerName = parts[1];
+                } else if (parts.length > 2) {
+                    playerName = parts[1] + " " + parts[2];
+                }
+
+                if(playerName.isEmpty()){
+                    System.out.println("Man, gib doch den Namen ein!!!");
+                }else{
+                    System.out.println("Spieler "+playerName+" erstellt!");
+                    Player newPlayer = new Player(playerName);
+                }
+                break;
+
+
+
+
+            default:
+                System.out.println("Ungültiger Befehl. Unter 'info' finden sie alle Befehle");
         }
     }
 }
